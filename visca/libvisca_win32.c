14195493ab29bd59bd301484442d5c7ec644cbb2
Mikko Koppanen <mkoppanen@php.net>
2011-04-21 10:09:45 +0100

    lowercase windows.h

--- visca/libvisca_win32.c
+++ visca/libvisca_win32.c
@@ -19,7 +19,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#include <Windows.h>
+#include <windows.h>
 #include <string.h>
 #include <fcntl.h>
 #include <errno.h>
