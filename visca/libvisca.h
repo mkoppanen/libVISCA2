8bb9cc127a803e95d6741e0efd525cc6ce8657a4
Mikko Koppanen <mkoppanen@php.net>
2011-05-12 13:01:38 +0100

    define int16_t as windows type

--- visca/libvisca.h
+++ visca/libvisca.h
@@ -405,6 +405,8 @@ typedef unsigned __int8 uint8_t;
 typedef unsigned __int16 uint16_t;
 typedef unsigned __int32 uint32_t;
 
+typedef __int16 int16_t;
+
 #  include <crtdbg.h>
 #else
 #  include <stdint.h>
